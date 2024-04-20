be5e2c8133e88117f806ae8ebd2a0a6883c5c07d
laszlo231409 <laszlo_14@hotmail.com>
2024-04-20 16:50:31 -0500

    agregar dependiente

---
+++ src/Clases/Dependiente.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Clases;
+
+/**
+ *
+ * @author Pruebas
+ */
+public class Dependiente {
+     private string nombre;
+     private int dni;
+}
